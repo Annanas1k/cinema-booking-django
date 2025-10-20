725a68d1067d1644d4a2c71758147d665fb31ed6
radisimo <radu.pavlovschi@gmail.com>
2025-10-20 18:35:49 +0300

    configure database conection

--- cinema_booking/settings.py
+++ cinema_booking/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'cinema_booking.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cinema_db',
+        'USER': 'postgres',
+        'PASSWORD': 'da',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- main1/views.py
+++ main1/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Hello, Cinema!</h1><p>Testul funcționează! 🎬</p>")
+
+
+def about(request):
+    return HttpResponse("da")
