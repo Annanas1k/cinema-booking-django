15e4c50866fcd635e0c8d48aae36d97d97f7f41c
radisimo <radu.pavlovschi@gmail.com>
2025-11-04 12:50:51 +0200

    add movies page and select all info

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('filme/', views.movie_list_view, name='movie_list'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -4,37 +4,23 @@ from django.db.models import Min, Count
 from .models import Movie, ShowTime
 
 def home_page(request):
-    """
-    Afișează pagina Home cu o listă a filmelor care au proiecții active,
-    corectând numele de relație inversă pentru adnotare.
-    """
+
     now = timezone.now()
 
-    # 1. Filtrează Showtimes: active și viitoare.
-    #    (Folosim .select_related('movie') pentru optimizare)
     active_showtimes = ShowTime.objects.filter(
         is_active=True,
     ).select_related('movie')
 
-    # 2. Obține ID-urile unice ale filmelor care au aceste proiecții.
     movie_ids = active_showtimes.values_list('id', flat=True).distinct()
 
-    # 3. Preluăm detaliile filmelor și le adnotăm.
-    #    Folosim "showtime_set" ca nume de relație inversă standard
-    #    (Dacă "showtime" nu a funcționat)
     movies_with_showtimes = Movie.objects.filter(id__in=movie_ids).annotate(
-        min_price=Min('showtime__price'), # <--- Am lăsat "showtime" (numele modelului în lc)
+        min_price=Min('showtime__price'),
     ).order_by('release_date')
 
-    # NOTĂ IMPORTANTĂ:
-    # Dacă *această* versiune (cu 'showtime') nu funcționează,
-    # înseamnă că trebuie să folosești numele implicit al relației inverse: 'showtime_set'.
-    # În acest caz, liniile 2 și 3 de mai sus ar trebui să fie:
-    #
+
     # min_price=Min('showtime_set__price'),
     # showtime_count=Count('showtime_set')
-    #
-    # Te rog să încerci ambele variante!
+
 
     context = {
         'movies': movies_with_showtimes,
@@ -43,3 +29,13 @@ def home_page(request):
     }
 
     return render(request, 'movies/home.html', context)
+
+
+def movie_list_view(request):
+    all_movies = Movie.objects.all().order_by('title')
+
+    context = {
+        'movies': all_movies,
+        'page_title': 'Toate Filmele',
+    }
+    return render(request, 'movies/movies_list.html', context)
